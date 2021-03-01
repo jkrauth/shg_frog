8a95094752e7236483a02cdde561388c76bf80b3
Julian Krauth <mail@j-krauth.de>
2021-03-01 21:46:36 +0100

    formatted code and added legend to plots

--- shg_frog/View/retrieval_window.py
+++ shg_frog/View/retrieval_window.py
@@ -24,7 +24,7 @@ class RetrievalGraphics:
         # Create the window
         self.win = pg.GraphicsWindow()
         self.win.setWindowTitle('Phase Retrieval - SHG FROG')
-        self.win.setGeometry(400,0,1000,1000)
+        self.win.setGeometry(400, 0, 1000, 1000)
         #self.win.move(300,0)
 
 
@@ -45,28 +45,32 @@ class RetrievalGraphics:
         self.win.nextRow()
         self.p3 = self.win.addPlot(colspan=2)
         self.p3.setLabel('left','|E|^2 & ang(E)')
+        self.p3.addLegend()
 
         # Add item to show pulse in frequency domain
         self.win.nextRow()
         self.p4 = self.win.addPlot(colspan=2)
         self.p4.setLabel('left','|E|^2 & ang(E)')
+        self.p4.addLegend()
         #self.p4.setXLink(self.p3)
         #self.p4.setYLink(self.p3)
 
-        # Create Colormaps from matplotlib colormaps
-        if 1:
-            colormap = cm.get_cmap("plasma")
-            colormap._init()
-            # Convert matplotlib colormap from 0-1 to 0-255 for Qt
-            lut = (colormap._lut * 255).view(np.ndarray)
-            lut = lut[1:-3] # Truncate array, for some reason it is too long
-            # Apply the colormap
-            self.img1.setLookupTable(lut)
-            self.img2.setLookupTable(lut)
+        # Colormap for FROG images
+        self.set_colormap()
 
 
-    #@pyqtSlot(np.ndarray,np.ndarray)
-    def set_axis(self,tpxls,vpxls):
+    def set_colormap(self):
+        """ Create a colormap for the FROG images """
+        colormap = cm.get_cmap("plasma")
+        colormap._init()
+        # Convert matplotlib colormap from 0-1 to 0-255 for Qt
+        lut = (colormap._lut * 255).view(np.ndarray)
+        lut = lut[1:-3] # Truncate array, for some reason it is too long
+        # Apply the colormap
+        self.img1.setLookupTable(lut)
+        self.img2.setLookupTable(lut)
+
+    def set_axis(self, tpxls: np.ndarray, vpxls: np.ndarray):
         """
         Sets axis attributes which are needed for x and y scale
         """
@@ -75,19 +79,18 @@ class RetrievalGraphics:
         self.tpxls = tpxls
         self.vpxls = vpxls
         # Set correct scalings for axis of FROG traces
-        self.img1.scale(tpxls[1]-tpxls[0],vpxls[1]-vpxls[0])
-        self.img2.scale(tpxls[1]-tpxls[0],vpxls[1]-vpxls[0])
+        self.img1.scale(tpxls[1]-tpxls[0], vpxls[1]-vpxls[0])
+        self.img2.scale(tpxls[1]-tpxls[0], vpxls[1]-vpxls[0])
         self.img1.translate(-N/2,-N/2) # center axes
         self.img2.translate(-N/2,-N/2) # center axes
         # Create initial graphs for showing the retrieved pulse
-        self.p3p = self.p3.plot(tpxls,np.zeros(N),pen=(255,0,0))
-        self.p3p2= self.p3.plot(tpxls,np.zeros(N),pen=(0,255,0))
-        self.p4p = self.p4.plot(vpxls,np.zeros(N),pen=(255,0,0))
-        self.p4p2= self.p4.plot(vpxls,np.zeros(N),pen=(0,255,0))
+        self.p3p = self.p3.plot(tpxls, np.zeros(N), pen=(255, 0, 0), name='Amplitude')
+        self.p3p2= self.p3.plot(tpxls, np.zeros(N), pen=(0, 255, 0), name='Phase')
+        self.p4p = self.p4.plot(vpxls, np.zeros(N), pen=(255, 0, 0), name='Amplitude')
+        self.p4p2= self.p4.plot(vpxls, np.zeros(N), pen=(0, 255, 0), name='Phase')
 
 
-    #@pyqtSlot(int,np.ndarray)
-    def update_graphics(self,which,data):
+    def update_graphics(self, which: int, data: np.ndarray):
         """
         Can only be used after window has been created and
         axes set by method set_axis()
@@ -96,35 +99,33 @@ class RetrievalGraphics:
             self.img1.setImage(data)
         if which==1: # Set reconstructed FROG trace
             self.img2.setImage(data)
-        if which==2: # Set pulse time data
-            self.p3p.setData(self.tpxls,data)
+        if which==2: # Set pulse time amplitude
+            self.p3p.setData(self.tpxls, data)
         if which==3: # Set pulse time phase
-            self.p3p2.setData(self.tpxls,data)
-        if which==4: # Set pulse freq data
-            self.p4p.setData(self.vpxls,data)
+            self.p3p2.setData(self.tpxls, data)
+        if which==4: # Set pulse freq amplitude
+            self.p4p.setData(self.vpxls, data)
         if which==5: # Set pulse time phase
-            self.p4p2.setData(self.vpxls,data)
+            self.p4p2.setData(self.vpxls, data)
 
 
-    #@pyqtSlot(list)
-    def update_labels(self,units):
+    def update_labels(self, units: list):
+        """ Update the labels with the corresponding units. """
         dtunit = units[0]
         dvunit = units[1]
-        self.p1.setLabel('bottom','Delay [%s]' % dtunit)
-        self.p1.setLabel('left','SH freq [%s]' % dvunit)
-        self.p2.setLabel('bottom','Delay [%s]' % dtunit)
-        self.p2.setLabel('left','SH freq [%s]' % dvunit)
-        self.p3.setLabel('bottom','Time [%s]' % dtunit)
-        self.p4.setLabel('bottom','Frequency [%s]' % dvunit)
+        self.p1.setLabel('bottom', 'Delay [%s]' % dtunit)
+        self.p1.setLabel('left', 'SH freq [%s]' % dvunit)
+        self.p2.setLabel('bottom', 'Delay [%s]' % dtunit)
+        self.p2.setLabel('left', 'SH freq [%s]' % dvunit)
+        self.p3.setLabel('bottom', 'Time [%s]' % dtunit)
+        self.p4.setLabel('bottom', 'Frequency [%s]' % dvunit)
 
     #@pyqtSlot(int,float)
-    def update_title(self,iteration,G):
-        self.p2.setTitle(title='Reconstructed: iter=%d G=%.4f'
-                         % (iteration,G))
+    def update_title(self, iteration: int, G: float):
+        """ Update the title to set the current iteration and G values. """
+        self.p2.setTitle(title='Reconstructed: iter=%d G=%.4f' % (iteration,G))
 
-    def screenshot(self,widget):
+    def screenshot(self, widget):
         exporter = pg.exporters.ImageExporter(widget)
         #exporter.parameters()['widht'] = 100
         exporter.export('screenshot.png')
-
-    """ End RetrievalGraphics class """
