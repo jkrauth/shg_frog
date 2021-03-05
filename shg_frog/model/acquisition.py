07564aacec7e7b9cde0836d0f461ab08ad47fa5f
Julian Krauth <mail@j-krauth.de>
2021-03-05 19:30:15 +0100

    completely rewritten, now using mixins

--- shg_frog/model/acquisition.py
+++ shg_frog/model/acquisition.py
@@ -11,154 +11,90 @@ Python Version: 3.7
 """
 import numpy as np
 
-from labdevices import ando as ANDO
-from labdevices import allied_vision
-
-class Spectrometer:
-    """This class defines the model for the Spectrometer."""
-
-    def __init__(self, test: bool=True):
-        if test:
-            self.ando = ANDO.SpectrumAnalyzerDummy()
-            self.camera = allied_vision.MantaDummy('DEV_000F314E1E59')
-        else:
-            self.ando = ANDO.SpectrumAnalyzer()
-            self.camera = allied_vision.Manta('DEV_000F314E1E59')
-        self.mode = None # Is set upon initialization
+from labdevices import ando, allied_vision
 
 
-    def initialize(self, mode: int) -> None:
-        """Connect to the device.
-        Argument:
-        mode - 0 for camera, 1 for ando
-        """
-        self.mode = mode
-        if mode == 0:
-            self.camera.initialize()
-        elif mode == 1:
-            self.ando.initialize()
-
-    def get_ando_spectrum(self):
-        """Get spectrum from Ando Spectrometer"""
-        self.ando.sweep()
-        self.ando.finish()
-        y_data = self.ando.get_y_data()
-        return y_data
-
-    def get_camera_spectrum(self):
+class _CameraMixin:
+    """ Extension/Mixin for the Manta camera class """
+    def get_spectrum(self) -> np.ndarray:
         """Get spectrum from ccd camera"""
-        img = self.camera.take_single_img()
+        img = self.take_single_img()
         # Project image onto a single axis and normalize
-        y_data = np.divide(np.sum(img,0), float(np.ma.size(img,0)))
+        y_data = np.divide(np.sum(img, 0), float(np.ma.size(img, 0)))
         return y_data
 
-    def get_spectrum(self):
-        """Get spectrum from the connected device (ando or ccd)"""
-        if self.mode == 0:
-            return self.get_camera_spectrum()
-        else:
-            return self.get_ando_spectrum()
+    def get_roi(self) -> list:
+        """ Gives the region of interest. """
+        img_format = [0]*4
+        img_format[0] = self.roi_x
+        img_format[1] = self.roi_y
+        img_format[2] = self.roi_dx
+        img_format[3] = self.roi_dy
+        return img_format
 
-    def close(self):
-        """Close connection to device."""
-        print(self.mode)
-        if self.mode == 0:
-            self.camera.close()
-        else:
-            self.ando.close()
-
-    def exposure(self, val=None):
-        """Set exposure of the camera"""
-        if val is None:
-            return self.camera.exposure
-        else:
-            self.camera.exposure = val
-
-    def gain(self, val=None):
-        """Set gain of the camera"""
-        if val is None:
-            return self.camera.gain
-        else:
-            self.camera.gain = val
-
-    def img_format(self, offsetx=None, offsety=None,
-            width=None, height=None):
+    def set_roi(self, offsetx=None, offsety=None,
+            width=None, height=None) -> None:
         """
-        Get/Set position and format of the image which is acquired
-        from the camera chip. (It can be just a fraction of the full
-        format)
-        Full format is [0, 0, 1936, 1216]
+        Set region of interest of the image which is acquired from the camera chip.
+        (It can be just a fraction of the full format)
         Units in pixels!
         """
-        if offsetx==offsety==width==height==None:
-            img_format = np.zeros(4,dtype=int)
-            img_format[0] = self.camera.roi_x
-            img_format[1] = self.camera.roi_y
-            img_format[2] = self.camera.roi_dx
-            img_format[3] = self.camera.roi_dy
-            return img_format
-        else:
-            if offsetx is not None:
-                self.camera.roi_x = offsetx
-            if offsety is not None:
-                self.camera.roi_y = offsety
-            if width is not None:
-                self.camera.roi_dx = width
-            if height is not None:
-                self.camera.roi_dy = height
+        if offsetx is not None:
+            self.roi_x = offsetx
+        if offsety is not None:
+            self.roi_y = offsety
+        if width is not None:
+            self.roi_dx = width
+        if height is not None:
+            self.roi_dy = height
 
-    def trig_source(self, source=None):
-        if source is None:
-            return self.camera.trig_source
-        else:
-            self.camera.trig_source = source
-
-    def pix_format(self, pix=None):
-        if pix is None:
-            return self.camera.pix_format
-        else:
-            self.camera.pix_format = pix
-
-    def img_format_full(self):
+    def img_format_full(self) -> None:
         """Set image format to full size of camera sensor"""
-        self.camera.roi_x = 0
-        self.camera.roi_y = 0
-        self.camera.roi_dx = self.camera.sensor_size[0]
-        self.camera.roi_dy = self.camera.sensor_size[1]
+        self.roi_x = 0
+        self.roi_y = 0
+        self.roi_dx = self.sensor_size[0]
+        self.roi_dy = self.sensor_size[1]
 
-    def take_single_img(self):
-        return self.camera.take_single_img()
-
-    def take_full_img(self):
+    def take_full_img(self) -> np.ndarray:
         """Saves current roi parameters, changes to full sensor size,
         takes full image, restores old roi parameters in the settings."""
-        x = self.camera.roi_x
-        y = self.camera.roi_y
-        dx = self.camera.roi_dx
-        dy = self.camera.roi_dy
+        x_old = self.roi_x
+        y_old = self.roi_y
+        dx_old = self.roi_dx
+        dy_old = self.roi_dy
         self.img_format_full()
-        img = self.take_single_img()
-        self.img_format(x, y, dx, dy)
-        return img
+        image = self.take_single_img()
+        self.set_roi(x_old, y_old, dx_old, dy_old)
+        return image
 
+    # Define callables needed for pyqt connect functions
+    def set_exposure(self, exposure):
+        self.exposure = exposure
 
-    def ctr(self, wl=None):
-        if wl is None:
-            return self.ando.ctr()
-        else:
-            self.ando.ctr(wl)
+    def set_gain(self, gain):
+        self.gain = gain
 
-    def span(self, span=None):
-        if span is None:
-            return self.ando.span()
-        else:
-            self.ando.span(span)
+    def set_trig_source(self, source):
+        self.trig_source = source
 
-    def cw_mode(self, cw=None):
-        if cw is None:
-            return self.ando.cw_mode()
-        else:
-            self.ando.cw_mode(cw)
+class Camera(allied_vision.Manta, _CameraMixin):
+    """ Manta camera with some additional features. """
 
-    def peak_hold_mode(self, time):
-        self.ando.peak_hold_mode(time)
+class CameraDummy(allied_vision.MantaDummy, _CameraMixin):
+    """ Manta camera dummy with some additional features """
+
+class _SpectrometerMixin:
+    """Extension/Mixin for the Spectrum analyzer."""
+
+    def get_spectrum(self):
+        """Get spectrum from Ando Spectrometer"""
+        self.do_sweep()
+        self.finish()
+        y_data = self.get_y_data()
+        return y_data
+
+class Spectrometer(ando.SpectrumAnalyzer, _SpectrometerMixin):
+    """ Spectrometer with additional features. """
+
+class SpectrometerDummy(ando.SpectrumAnalyzerDummy, _SpectrometerMixin):
+    """ Spectrometer Dummy with additional features. """
