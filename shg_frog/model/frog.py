a8c95839e3d3032ea93cfd6ff1eeadb58ee50721
Julian Krauth <mail@j-krauth.de>
2021-03-17 11:20:38 +0100

    data and config files are now saved in folders in the home directory

--- shg_frog/model/frog.py
+++ shg_frog/model/frog.py
@@ -19,9 +19,22 @@ from labdevices import newport
 from . import acquisition
 from . import phase_retrieval
 
-MAIN_DIR = pathlib.Path(__file__).parents[2]
-CONFIG_DIR = MAIN_DIR / "Config"
-DATA_DIR = MAIN_DIR / "Data"
+HOME_DIR = pathlib.Path.home()
+# These folders will be created if not existent.
+CONFIG_DIR = HOME_DIR / ".frog_config"
+DATA_DIR = HOME_DIR / "frog_data"
+
+DEFAULT_CONFIG = {
+    "camera model": "Manta G-234B NIR",
+    "camera id": "DEV_000F314E1E59", # Necessary to select the camera
+    "stage port": "/dev/ttyUSB0",
+    "pixel size": 5.86, # Given in micron
+    "pxls height": 1216, # Number of pixels in vertical
+    "pxls width": 1936, # Number of pixels in horizontal
+    "center wavelength": 345, # in nm, center wavelength of the pulse
+    "focal length": 200, # in mm, lens between grating and camera
+    "grating": 0.81, # Grating specified with 0.81nm/mrad
+}
 
 SPEEDOFLIGHT = 299792458. #m/s
 
@@ -448,8 +461,8 @@ class FileHandler:
     """ Saving and loading data into and from files. """
     def __init__(self):
         self.name_meta = 'meta.yml'
-        self.name_config = 'config.yml'
         self.name_frog = 'frog.tiff'
+        self.name_config = 'config.yml'
 
     def _get_new_measurement_path(self) -> pathlib.Path:
         """Returns a path for the next measurement."""
@@ -489,16 +502,27 @@ class FileHandler:
         imageio.imsave(measurement_path / self.name_frog, data.image.astype(bit_type))
         # Save settings
         with open(measurement_path / self.name_meta, 'w') as f:
-            f.write(yaml.dump(data.meta, default_flow_style=False))
+            yaml.dump(data.meta, f, default_flow_style=False)
         # Save configuration
         with open(measurement_path / self.name_config, 'w') as f:
-            f.write(yaml.dump(config, default_flow_style=False))
+            yaml.dump(config, f, default_flow_style=False)
 
     def get_main_config(self) -> dict:
-        """Get defaults from configuration file."""
-        with open(CONFIG_DIR / self.name_config, 'r') as f:
+        """Load default from custom file if it exists, otherwise create file and
+        return default config."""
+        config_path = CONFIG_DIR / self.name_config
+        if not CONFIG_DIR.exists():
+            CONFIG_DIR.mkdir()
+            with open(config_path, 'w') as f:
+                yaml.dump(DEFAULT_CONFIG, f, default_flow_style=False)
+            return DEFAULT_CONFIG
+        with open(config_path, 'r') as f:
             config = yaml.load(f, Loader=yaml.FullLoader)
         return config
+
+    def save_main_config(self, config: dict):
+        """ Save current config to file in CONFIG_DIR """
+        pass
 
     def get_measurement_data(self, measurement_path: pathlib.Path) -> Data:
         """ Get config, settings (meta), and image data of an old measurement. """
